f5bfa757e0a919d1968d9a00bfb3cc7cc6494301
Venkteshkavi <venktesh@buffalo.edu>
2019-12-22 04:07:17 -0500

    Added Sliding Window Template

---
+++ Trees/slidingWindowTemplate.java
@@ -0,0 +1,63 @@
+/* LEETCODE PROBLEMS - USE CASE OF THE TEMPLATE (LEETCODE)
+  1. Minimum Window substring
+  2. longest substring without repeating characters
+  3. Longest Substring with At Most Two Distinct Characters
+  4. Longest Substring with At Most K Distinct Characters
+  5. Substring with Concatenation of All Words
+  6. Find All Anagrams in a String
+  7. Longest Repeating Character Replacement
+*/
+
+
+// PATTERN MATCHING TEMPLATE
+
+public class Solution {
+    public List<Integer> slidingWindowTemplateByHarryChaoyangHe(String s, String t) {
+        //init a collection or int value to save the result according the question.
+        List<Integer> result = new LinkedList<>();
+        if(t.length()> s.length()) return result;
+
+        //create a hashmap to save the Characters of the target substring.
+        //(K, V) = (Character, Frequence of the Characters)
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        //maintain a counter to check whether match the target string.
+        int counter = map.size();//must be the map size, NOT the string size because the char may be duplicate.
+
+        //Two Pointers: begin - left pointer of the window; end - right pointer of the window
+        int begin = 0, end = 0;
+
+        //the length of the substring which match the target string.
+        int len = Integer.MAX_VALUE;
+
+        //loop at the begining of the source string
+        while(end < s.length()){
+
+            char c = s.charAt(end);//get a character
+
+            if( map.containsKey(c) ){
+                map.put(c, map.get(c)-1);// plus or minus one
+                if(map.get(c) == 0) counter--;//modify the counter according the requirement(different condition).
+            }
+            end++;
+
+            //increase begin pointer to make it invalid/valid again
+            while(counter == 0 /* counter condition. different question may have different condition */){
+
+                char tempc = s.charAt(begin);//***be careful here: choose the char at begin pointer, NOT the end pointer
+                if(map.containsKey(tempc)){
+                    map.put(tempc, map.get(tempc) + 1);//plus or minus one
+                    if(map.get(tempc) > 0) counter++;//modify the counter according the requirement(different condition).
+                }
+
+                /* save / update(min/max) the result if find a target*/
+                // result collections or result int value
+
+                begin++;
+            }
+        }
+        return result;
+    }
+}
