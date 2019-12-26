09ace3af1b3e8aff8da8b7df07251d8b1420c844
Venkteshkavi <venktesh@buffalo.edu>
2019-12-26 12:01:10 -0500

    "added pathsum2"

---
+++ Trees/pathSum2.java
@@ -0,0 +1,25 @@
+class Solution{
+    public List<List<Integer>> pathSum(TreeNode root,int sum){
+        List<List<Integer>> res = new ArrayList();
+        List<Integer> path = new ArrayList();
+        helper(res,path,root,sum);
+
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res,List<Integer> path,TreeNode root,int sum){
+        if(root == null){
+            return;
+        }
+
+        sum -= root.val;
+        path.add(root.val);
+        if(root.left == null && root.right == null && sum == 0){
+            res.add(new ArrayList(path));
+        }
+        helper(res,path,root.left,sum);
+        helper(res,path,root.right,sum);
+
+        path.remove(path.size()-1);
+    }
+}
